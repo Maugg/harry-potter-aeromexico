ba65617c0e6c04f38445630edc4256817f1805c1
MauricioGG <mauriciogg@codigo-latte.com>
2022-01-13 14:58:20 -0600

    se agrega el archivo yarn log al git ignore se cambia el codigo de App.tsx  y se agregan tipos de datos el el archivo react-app-env.d.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Home from './Components/Pages/Home/Home';
+import "./Styles/normalize.css";
+import "./Styles/styles.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Home/>
+      
+    </>
   );
 }
 
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,26 @@
 /// <reference types="react-scripts" />
+type Gender = "male" | "female"
+type House = "Gryffindor" | "Slytherin" | "Ravenclaw" | "Hufflepuff"
+
+type Character = { name: string, species: string, gender: Gender, house: House, dateOfBirth: string, yearOfBirth: number, ancestry: string, eyeColour: string, hairColour: string, wand: { wood: string, core: string, length: number }, patronus: string, hogwartsStudent: boolean, hogwartsStaff: boolean, actor: string, alive: boolean, image: string }
+
+
+type CardData = {
+    name:string
+    dateOfBirth:string,
+    gender:Gender,
+    eyesColour:string,
+    hairColour:string,
+    house:House,
+    image:string,
+    alive:boolean,
+    typeCharacter: string
+}
+
+type RequestFetch = "GET" | "POST" | "PUT" | "DELETE" | "PATCH";
+
+type RequestBody = {
+    method?: RequestFetch,
+    headers: Headers,
+    body: string | FormData;
+}
