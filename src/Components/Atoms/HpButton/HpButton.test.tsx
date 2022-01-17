5369ca522bf06a5690b743a4216c462e99c01911
MauricioGG <mauriciogg@codigo-latte.com>
2022-01-17 02:29:29 -0600

    se agregan test unitarios a algunos componentes

--- src/Components/Atoms/HpButton/HpButton.test.tsx
+++ src/Components/Atoms/HpButton/HpButton.test.tsx
@@ -1,1 +1,25 @@
-export{}
+import React from "react";
+import ReactDOM from "react-dom";
+import HpButton from "./HpButton";
+import {fireEvent, render} from "@testing-library/react";
+
+describe("Prueba de boton", () => {
+
+    it("Renderizado correcto de las propiedades tipo string", () => {
+        let dataset: string | null = "hola", text: string = "boton", className: string | null = "clase-btn";
+        const wrapper = render(<HpButton text={text} className={className} dataset={dataset}  />);
+        wrapper.getByText("boton");
+        expect(wrapper.getByRole("button")).toHaveClass("clase-btn");
+        expect(wrapper.getByRole("button").dataset.id).toEqual("hola");
+    });
+
+    it("click en el boton con funcion enlazadora",()=>{
+        const handleFuntion = jest.fn();
+        const wrapper = render(<HpButton text="hola" handleFuntion={handleFuntion}  />);
+        fireEvent.click(wrapper.getByText("hola"))
+        expect(handleFuntion.mock.calls).toHaveLength(1);
+
+    });
+
+
+})
--- src/Components/Molecules/HpInput/HpInput.test.tsx
+++ src/Components/Molecules/HpInput/HpInput.test.tsx
@@ -1,1 +1,35 @@
-export{}
+import HpInput from "./HpInput";
+import {fireEvent, render} from "@testing-library/react";
+
+describe("Componente HpInput",()=>{
+    let labelText = "label text"
+    it("rendizar input type[text]",()=>{
+        
+        const wrapper = render(<HpInput type="text" textLabel="label text" />);
+        let data = wrapper.container.querySelector("input") as HTMLInputElement;
+        expect(data.type).toEqual("text");
+    });
+
+    it("rendizar input type[date]",()=>{
+        
+        const wrapper = render(<HpInput type="date" textLabel="label text" />);
+        let data = wrapper.container.querySelector("input") as HTMLInputElement;
+        expect(data.type).toEqual("date");
+    });
+
+    it("rendizar input type[radio]",()=>{
+        
+        const wrapper = render(<HpInput type="radio" textLabel="label text" />);
+        let data = wrapper.container.querySelector("input") as HTMLInputElement;
+        expect(data.type).toEqual("radio");
+    });
+
+    it("rendizar input type[file]",()=>{
+        
+        const wrapper = render(<HpInput type="file" textLabel="label text" />);
+        let data = wrapper.container.querySelector("input") as HTMLInputElement;
+        expect(data.type).toEqual("file");
+    });
+
+   
+});
